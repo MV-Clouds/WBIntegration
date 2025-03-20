b511f9acc4104958fa46fbac2f5b7ba4a00cbe81
Mitrajsinh Gohil <122979975+MRG6804@users.noreply.github.com>
2025-03-20 15:02:26 +0530

    Update objectConfigComp.js

--- force-app/main/default/lwc/objectConfigComp/objectConfigComp.js
+++ force-app/main/default/lwc/objectConfigComp/objectConfigComp.js
@@ -1,4 +1,4 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track } from 'lwc';
 import getRequiredFields from '@salesforce/apex/ObjectConfigController.getRequiredFields';
 import getTextAndPhoneFields from '@salesforce/apex/ObjectConfigController.getTextAndPhoneFields';
 import getUserConfig from '@salesforce/apex/ObjectConfigController.getUserConfig';
